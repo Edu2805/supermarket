541595374256ed9db15a7c79cee8c96cc1129315
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2022-12-30 17:12:36 -0300

    #feature/25 - Ajustes no mapper para inverção de dependência

--- src/main/java/br/com/amorim/supermarket/controller/productdata/dto/ConvertProductMapperImpl.java
+++ src/main/java/br/com/amorim/supermarket/controller/productdata/dto/ConvertProductMapperImpl.java
@@ -2,9 +2,12 @@ package br.com.amorim.supermarket.controller.productdata.dto;
 
 import br.com.amorim.supermarket.model.productdata.ProductData;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+/**
+ * Classe que gera o DTO de ProductData
+ */
 
 @AllArgsConstructor
 
@@ -13,6 +16,12 @@ public class ConvertProductMapperImpl implements ConvertProductMapper{
 
     private ModelMapper modelMapper;
 
+    /**
+     * Classe que irá converter o ProductData para o seu DTO para cadastro de
+     * produtos
+     * @param productDTO produto cadastrado
+     * @return ProductData mapeado para ProductDTO
+     */
     @Override
     public ProductData createProductMapper(ProductDTO productDTO) {
         return modelMapper.map(productDTO, ProductData.class);
