4702bdf9d1d8fddd2eee3432e386c83d5eed66cd
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-01-07 15:39:56 -0300

    #documentation/02 - Configuração do mirror repository para o github

--- src/main/java/br/com/amorim/supermarket/controller/productdata/dto/ProductDTO.java
+++ src/main/java/br/com/amorim/supermarket/controller/productdata/dto/ProductDTO.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
