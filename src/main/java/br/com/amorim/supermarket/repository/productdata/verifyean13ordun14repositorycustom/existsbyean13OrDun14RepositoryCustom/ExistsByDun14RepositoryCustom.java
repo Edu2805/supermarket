853b8c6d7d13922ac557188bd7b8a6ecd69328ed
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-01-08 12:29:10 -0300

    #feature/40 - Simplificação da classe VerifyEan13OrDun14RepositoryCustom

--- src/main/java/br/com/amorim/supermarket/repository/productdata/verifyean13ordun14repositorycustom/existsbyean13OrDun14RepositoryCustom/ExistsByDun14RepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package br.com.amorim.supermarket.repository.productdata.verifyean13ordun14repositorycustom.existsbyean13OrDun14RepositoryCustom;
-
-public interface ExistsByDun14RepositoryCustom {
-
-    boolean existsByDun14 (String dun14);
-}
