dc69ab03bc996064553342af46fa5916284fd107
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-01-28 17:41:09 -0300

    #feature/70 - Implementação do DTO de person e criação de chaves de tradução

---
+++ src/main/java/br/com/amorim/supermarket/controller/person/dto/ConverterPersonMapperImpl.java
@@ -0,0 +1,19 @@
+package br.com.amorim.supermarket.controller.person.dto;
+
+import br.com.amorim.supermarket.model.person.Person;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+
+@Component
+public class ConverterPersonMapperImpl implements ConverterPersonMapper {
+
+    private ModelMapper modelMapper;
+
+    @Override
+    public Person createOrUpdatePersonMapper(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
+    }
+}
