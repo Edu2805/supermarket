6d2dc3077111c5e3e748792da22f16b8800ed306
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-01-28 16:18:39 -0300

    #feature/69 - Finalização dos testes em Establishmente e classe utilitária de controle de paginação

---
+++ src/test/java/br/com/amorim/supermarket/common/verifypagesize/VerifyPageSizeImplTest.java
@@ -0,0 +1,49 @@
+package br.com.amorim.supermarket.common.verifypagesize;
+
+import br.com.amorim.supermarket.common.enums.MessagesKeyType;
+import br.com.amorim.supermarket.common.exception.invalidactionexception.InvalidActionException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static br.com.amorim.supermarket.configuration.internacionalizationmessages.ResourcesBundleMessages.getString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+@SpringBootTest
+class VerifyPageSizeImplTest {
+
+    @InjectMocks
+    private VerifyPageSizeImpl verifyPageSize;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+    }
+
+    @Test
+    void shouldShowAInvalidActionExceptionMessageWhenPageSizeIsGreaterThanTwenty() {
+        int size = 21;
+        var errorMessage = getString(MessagesKeyType.COMMON_PAGE_SIZE_INVALID_PAGE_SIZE.message);
+
+        String exceptionMessage = Assertions.assertThrows(
+                InvalidActionException.class, () -> {
+                    verifyPageSize.verifyPageSizeForGetAll(size);
+                }
+        ).getMessage();
+        assertEquals(errorMessage, exceptionMessage);
+        assertThrows(InvalidActionException.class, () ->
+                verifyPageSize.verifyPageSizeForGetAll(size));
+    }
+
+    @Test
+    void shouldReturnTrueWhenPageSizeIsLessOrEqualsThanTwenty() {
+        int size = 20;
+        var lessThanTwenty = verifyPageSize.verifyPageSizeForGetAll(size);
+        assertFalse(lessThanTwenty);
+    }
+}
