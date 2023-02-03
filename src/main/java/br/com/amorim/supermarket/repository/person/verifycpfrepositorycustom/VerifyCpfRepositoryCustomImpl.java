24853e5b38e579329001fa8a58c627db671c55c4
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-02-02 22:07:20 -0300

    #feature/73 - Ajustes code review

--- src/main/java/br/com/amorim/supermarket/repository/person/verifycpfrepositorycustom/VerifyCpfRepositoryCustomImpl.java
+++ src/main/java/br/com/amorim/supermarket/repository/person/verifycpfrepositorycustom/VerifyCpfRepositoryCustomImpl.java
@@ -2,7 +2,6 @@ package br.com.amorim.supermarket.repository.person.verifycpfrepositorycustom;
 
 import br.com.amorim.supermarket.model.person.Person;
 import br.com.amorim.supermarket.model.person.QPerson;
-import br.com.amorim.supermarket.model.productdata.ProductData;
 import com.querydsl.jpa.impl.JPAQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -19,7 +18,7 @@ public class VerifyCpfRepositoryCustomImpl implements VerifyCpfRepositoryCustom 
     @Override
     public boolean isCpfAlreadyExistsInTheDatabase(Person person) {
         QPerson qPerson = QPerson.person;
-        JPAQuery<ProductData> query = new JPAQuery<>(entityManager);
+        JPAQuery<Person> query = new JPAQuery<>(entityManager);
         return !query.select(qPerson)
                 .from(qPerson)
                 .where(qPerson.cpf.eq(person.getCpf()))
