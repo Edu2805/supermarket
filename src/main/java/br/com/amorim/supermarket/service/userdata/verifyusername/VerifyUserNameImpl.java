373ce824e6a31069061289edd935da9c82e55ec3
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-02-07 21:38:28 -0300

    #feature/91 - Implementação do update de UserData e testes unitários

--- src/main/java/br/com/amorim/supermarket/service/userdata/verifyusername/VerifyUserNameImpl.java
+++ src/main/java/br/com/amorim/supermarket/service/userdata/verifyusername/VerifyUserNameImpl.java
@@ -3,6 +3,7 @@ package br.com.amorim.supermarket.service.userdata.verifyusername;
 import br.com.amorim.supermarket.common.enums.MessagesKeyType;
 import br.com.amorim.supermarket.common.exception.businessrule.BusinessRuleException;
 import br.com.amorim.supermarket.model.userdata.UserData;
+import br.com.amorim.supermarket.repository.userdata.UserDataRepository;
 import br.com.amorim.supermarket.repository.userdata.verifyusernamerepositorycustom.VerifyUserNameRepositoryCustom;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import static br.com.amorim.supermarket.configuration.internacionalizationmessag
 public class VerifyUserNameImpl implements VerifyUserName {
 
     private VerifyUserNameRepositoryCustom verifyUserNameRepositoryCustom;
+    private UserDataRepository userDataRepository;
 
     @Override
     public boolean verifyUserDataBeforeSave(UserData userData) {
@@ -27,6 +29,16 @@ public class VerifyUserNameImpl implements VerifyUserName {
 
     @Override
     public boolean verifyUserDataBeforeUpdate(UserData userData) {
+        var getUsers = userDataRepository.findAll();
+        getUsers.forEach(user -> {
+            if (user.getUserName() != null && (
+                    user.getUserName().equals(userData.getUserName()))) {
+                if (!user.getId().equals(userData.getId())) {
+                    throw new BusinessRuleException(getString(
+                            MessagesKeyType.USER_DATA_USER_NAME_ALREADY_EXISTS_WHEN_UPDATE.message));
+                }
+            }
+        });
         return false;
     }
 }
