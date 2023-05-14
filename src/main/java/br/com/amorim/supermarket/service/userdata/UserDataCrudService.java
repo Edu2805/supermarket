62d5b4b445d1394d589c6fcb2334804d29761ab4
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-05-14 17:08:13 -0300

    #feature/139 - Implementação de novo endpoint para busca da role pelo nome do usuário

--- src/main/java/br/com/amorim/supermarket/service/userdata/UserDataCrudService.java
+++ src/main/java/br/com/amorim/supermarket/service/userdata/UserDataCrudService.java
@@ -3,7 +3,6 @@ package br.com.amorim.supermarket.service.userdata;
 import br.com.amorim.supermarket.model.userdata.UserData;
 import br.com.amorim.supermarket.service.common.genericcrudservice.CrudServiceCommon;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserDataCrudService extends CrudServiceCommon<UserData, UUID> {
