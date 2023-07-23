3437c2bea77974cbbeb3a62b60c150b0b65878cf
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-07-22 21:44:08 -0300

    #bugfix/12 - Ocultando as APIs de attachment e roles

--- src/main/java/br/com/amorim/supermarket/controller/attachment/AttachmentController.java
+++ src/main/java/br/com/amorim/supermarket/controller/attachment/AttachmentController.java
@@ -1,11 +1,6 @@
 package br.com.amorim.supermarket.controller.attachment;
 
 import br.com.amorim.supermarket.service.attachment.AttachmentCrudService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,46 +9,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 
 import java.io.IOException;
 import java.util.UUID;
 
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @AllArgsConstructor
 
 @RestController
 @RequestMapping("api/attachment")
-@Api("Attachment")
 public class AttachmentController {
 
     private AttachmentCrudService attachmentCrudService;
 
     @PostMapping
-    @ResponseStatus(CREATED)
-    @ApiOperation("Save a attachment")
-    @ApiResponses({
-            @ApiResponse(code = 201, message = "Attachment successfully saved"),
-            @ApiResponse(code = 400, message = "An error occurred while saving the attachment")
-    })
-    public ResponseEntity<?> uploadFile (@RequestParam("image") @Valid @ApiParam("Parameters for saving the attachment") MultipartFile file) throws IOException {
+    @ApiIgnore
+    public ResponseEntity<?> uploadFile (@RequestParam("image") @Valid MultipartFile file) throws IOException {
         String uploadFile = attachmentCrudService.uploadFile(file);
         return ResponseEntity.status(OK).body(uploadFile);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("Get a specific attachment")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "Attachment returned successfully"),
-            @ApiResponse(code = 404, message = "Attachment not found for given file name")
-    })
-    public ResponseEntity<?> downloadFile (@PathVariable @ApiParam("Attachment id") UUID id) throws IOException {
+    @ApiIgnore
+    public ResponseEntity<?> downloadFile (@PathVariable UUID id) throws IOException {
         byte[] images = attachmentCrudService.downloadFileById(id);
         return ResponseEntity.status(OK).contentType(MediaType.valueOf("image/jpeg")).body(images);
     }
--- src/main/java/br/com/amorim/supermarket/controller/role/RoleController.java
+++ src/main/java/br/com/amorim/supermarket/controller/role/RoleController.java
@@ -2,14 +2,11 @@ package br.com.amorim.supermarket.controller.role;
 
 import br.com.amorim.supermarket.controller.role.dto.RoleOutput;
 import br.com.amorim.supermarket.service.role.RoleCrudService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +15,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/role")
-@Api("Role")
 public class RoleController {
 
     private RoleCrudService roleCrudService;
 
     @GetMapping
-    @ApiOperation("Get all roles")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "Roles returned successfully"),
-            @ApiResponse(code = 404, message = "An error occurred while fetching the roles")
-    })
+    @ApiIgnore
     public RoleOutput findAll () {
         List<String> roleName = new ArrayList<>();
         roleCrudService.findAllRoles().forEach(role -> roleName.add(role.getName()));
