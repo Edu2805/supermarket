4c553f8e95e33794807f09c06403b5ce924a0c9d
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2024-05-11 10:38:42 -0300

    #bugfix/19 - Ajustes para permitir que seja vendida mercadorias com estoque negativo

--- src/main/java/br/com/amorim/supermarket/service/goodsissue/registerproduct/RegisterProductImpl.java
+++ src/main/java/br/com/amorim/supermarket/service/goodsissue/registerproduct/RegisterProductImpl.java
@@ -1,7 +1,5 @@
 package br.com.amorim.supermarket.service.goodsissue.registerproduct;
 
-import br.com.amorim.supermarket.common.enums.MessagesKeyType;
-import br.com.amorim.supermarket.common.exception.notfound.NotFoundException;
 import br.com.amorim.supermarket.model.goodsissue.GoodsIssue;
 import br.com.amorim.supermarket.model.productdata.ProductData;
 import br.com.amorim.supermarket.repository.productdata.ProductDataRepository;
@@ -9,9 +7,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-
-import static br.com.amorim.supermarket.configuration.internacionalizationmessages.ResourcesBundleMessages.getString;
-import static java.lang.String.format;
 
 @AllArgsConstructor
 
@@ -34,15 +29,6 @@ public class RegisterProductImpl implements RegisterProduct {
     }
 
     private BigDecimal subtractStock(ProductData findProductData, ProductData productData) {
-        var inventoryUpdated = findProductData.getInventory().subtract(productData.getInventory());
-        if (inventoryUpdated.compareTo(BigDecimal.ZERO) < 0) {
-            if (findProductData.getDun14() == null) {
-                throw new NotFoundException(format(getString(MessagesKeyType.GOODS_ISSUE_PRODUCT_DATA_INSUFFICIENT_STOCK.message),
-                        findProductData.getName(), findProductData.getEan13()));
-            }
-            throw new NotFoundException(format(getString(MessagesKeyType.GOODS_ISSUE_PRODUCT_DATA_INSUFFICIENT_STOCK.message),
-                    findProductData.getName(), findProductData.getDun14()));
-        }
         return findProductData.getInventory().subtract(productData.getInventory());
     }
 }
--- src/test/java/br/com/amorim/supermarket/testutils/generateentitiesunittests/establishment/EstablishmentTest.java
+++ src/test/java/br/com/amorim/supermarket/testutils/generateentitiesunittests/establishment/EstablishmentTest.java
@@ -1,6 +1,5 @@
 package br.com.amorim.supermarket.testutils.generateentitiesunittests.establishment;
 
-import br.com.amorim.supermarket.model.attatchment.Attachment;
 import br.com.amorim.supermarket.model.establishment.Establishment;
 import br.com.amorim.supermarket.testutils.generatedocument.GenerateCNPJ;
 
@@ -17,8 +16,6 @@ public class EstablishmentTest {
         Random randomStateRegistration = new Random();
         Random randomManager = new Random();
         GenerateCNPJ generateCNPJ = new GenerateCNPJ();
-        Attachment attachment = new Attachment();
-        attachment.setId(UUID.randomUUID());
         var name = randomName.nextInt(10000, 19999);
         var code = randomCode.nextInt(1, 19999);
         var municipalRegistration = randomMunicipalRegistration.nextInt(1, 1999);
@@ -35,7 +32,6 @@ public class EstablishmentTest {
         establishment.setMunicipalRegistration(String.valueOf(municipalRegistration));
         establishment.setStateRegistration(String.valueOf(stateRegistration));
         establishment.setPhone("4833333333");
-        establishment.setEstablismentLogo(attachment);
         return establishment;
     }
 }
